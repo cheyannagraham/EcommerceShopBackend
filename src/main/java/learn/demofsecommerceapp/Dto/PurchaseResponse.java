17378db3c1634a3a7762ec0db8cfce013436cfc4
chey <cheyanna.graham@gmail.com>
2025-01-08 12:18:31 -0800

    add purchase response dto

---
+++ src/main/java/learn/demofsecommerceapp/Dto/PurchaseResponse.java
@@ -0,0 +1,13 @@
+package learn.demofsecommerceapp.Dto;
+
+public class PurchaseResponse {
+    private long orderTrackingNumber;
+
+    public long getOrderTrackingNumber() {
+        return orderTrackingNumber;
+    }
+
+    public void setOrderTrackingNumber(long orderTrackingNumber) {
+        this.orderTrackingNumber = orderTrackingNumber;
+    }
+}
