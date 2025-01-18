e751c94ac81c1cbc88479d8e72f0791ee555ec5e
chey <cheyanna.graham@gmail.com>
2025-01-17 16:32:47 -0800

    add dynamic port

--- src/main/java/learn/demofsecommerceapp/DemoFSecommerceappApplication.java
+++ src/main/java/learn/demofsecommerceapp/DemoFSecommerceappApplication.java
@@ -3,66 +3,28 @@ package learn.demofsecommerceapp;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import learn.demofsecommerceapp.Dto.PurchaseDto;
 import learn.demofsecommerceapp.Service.CheckoutService;
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoFSecommerceappApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(DemoFSecommerceappApplication.class, args);
-        String purchaseData = """
-                {
-                   "customer":{
-                      "firstName":"afasa",
-                      "lastName":"afasa",
-                      "email":"afasa@test.com"
-                   },
-                   "shippingAddress":{
-                      "street":"afasa",
-                      "city":"afasa",
-                      "state":"Alberta",
-                      "country":"Canada",
-                      "zipCode":"afasa"
-                   },
-                   "billingAddress":{
-                      "street":"fsfsf",
-                      "city":"sfdsf",
-                      "state":"Acre",
-                      "country":"Brazil",
-                      "zipCode":"19111"
-                   },
-                   "order":{
-                      "totalPrice":36.98,
-                      "totalQuantity":2
-                   },
-                   "orderItems":[
-                      {
-                         "imageUrl":"assets/images/products/coffeemugs/coffeemug-luv2code-1000.png",
-                         "quantity":1,
-                         "unitPrice":18.99,
-                         "productId":26
-                      },
-                      {
-                         "imageUrl":"assets/images/products/mousepads/mousepad-luv2code-1000.png",
-                         "quantity":1,
-                         "unitPrice":17.99,
-                         "productId":51
-                      }
-                   ]
-                }
-                """;
+        SpringApplication.run(DemoFSecommerceappApplication.class, args);
+    }
 
-        CheckoutService checkoutService = context.getBean(CheckoutService.class);
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            PurchaseDto purchaseDto = objectMapper.readValue(purchaseData, PurchaseDto.class);
-            System.out.println(checkoutService.checkout(purchaseDto));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+    @Bean
+    public TomcatServletWebServerFactory tomcat() {
+        return new TomcatServletWebServerFactory(){
+            @Override
+            protected void customizeConnector(Connector connector){
+                connector.setPort(Integer.parseInt(System.getenv("PORT")));
+            }
+        };
     }
 
 }
